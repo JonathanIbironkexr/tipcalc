97e0da188855bf4ed95c6cfd678cfd976eeb2845
user142219 <user142219@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-08-27 16:45:05 -0400

    Official First Commit

--- Tip Calculator v3/ViewController.swift
+++ Tip Calculator v3/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var originalchargefield: UITextField!
+    @IBOutlet weak var tiplabel: UILabel!
+    @IBOutlet weak var totallabel: UILabel!
+    @IBOutlet weak var tippercentbuttons: UISegmentedControl!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTapout(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func onTapout2(_ sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcthetip(_ sender: AnyObject) {
+        
+        let tippercents = [0.05, 0.10, 0.15, 0.20, 0.25]
+        
+        let bill = Double(originalchargefield.text!) ?? 0
+        let tip = bill * tippercents[tippercentbuttons.selectedSegmentIndex]
+        let Total = bill + tip
+        
+        tiplabel.text = String (format: "$%.2f", tip)
+        totallabel.text = String (format: "$%.2f", Total)    }
 }
 
